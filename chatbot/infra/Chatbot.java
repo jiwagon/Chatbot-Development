a3ba4904539977707dd08bcf771d79e699632215
jiwagon <jiwagon11@gmail.com>
2022-12-18 03:50:47 -0500

    Slotfiller update

--- chatbot/infra/Chatbot.java
+++ chatbot/infra/Chatbot.java
@@ -1,8 +1,17 @@
 package chatbot.infra;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
 import chatbot.component.DomainClassifier;
-import chatbot.component.WeatherIntentClassifier;
 import chatbot.component.FoodIntentClassifier;
+import chatbot.component.WeatherIntentClassifier;
+//import chatbot.component.FoodIntentClassifier;
+
+//=====Code Added for Assignment 4 (Language Understanding) Begins=====
+import chatbot.component.SlotFiller;
+//=====Code Added for Assignment 4 (Language Understanding) Ends=====
 
 public class Chatbot {
 	
@@ -21,6 +30,15 @@ public class Chatbot {
 	//=====Code Added for Assignment 3 (Language Understanding) Ends=====
 	
 	
+	//=====Code Added for Assignment 4 (Language Understanding) Begins=====
+		
+		//Don't forget to add the following line to the top of this Java file:
+		//import chatbot.component.SlotFiller;
+		
+		private SlotFiller nowSlotFiller;
+		
+	//=====Code Added for Assignment 4 (Language Understanding) Ends=====
+		
 	public Chatbot(String userName, String botName) {
 		
 		this.userName = userName;
@@ -30,10 +48,16 @@ public class Chatbot {
 		
 			this.nowDomainClassifier = new DomainClassifier();
 			this.weatherIntentClassifier = new WeatherIntentClassifier();
-			this.foodIntentClassifier = new FoodIntentClassifier();
+			//this.foodIntentClassifier = new FoodIntentClassifier();
 		
 		//=====Code Added for Assignment 3 (Language Understanding) Ends=====
 		
+		//=====Code Added for Assignment 4 (Language Understanding) Begins=====
+			
+			this.nowSlotFiller = new SlotFiller();
+			
+		//=====Code Added for Assignment 4 (Language Understanding) Ends=====
+			
 	}
 	
 	/*
@@ -51,15 +75,19 @@ public class Chatbot {
 		
 		System.out.println("--------------");
 		System.out.println("User Message: "+nowInputText);
-		String nowDomain = nowDomainClassifier.getLabel(nowInputText);  //return domain name 
+		String nowDomain = nowDomainClassifier.getLabel(nowInputText);
 		System.out.println("Domain: "+nowDomain);
 		String nowIntent = "";
 		
-		// Logic: if domain is not "Other", it will point to weather or food 
+		
+		//=====Code Added for Assignment 4 (Language Understanding) Begins=====
+		Hashtable<String, String> extractedSlotValues = nowSlotFiller.extractSlotValues(nowInputText);
+		//=====Code Added for Assignment 4 (Language Understanding) Ends=====
+		
 		if(!nowDomain.equals("Other")) {//in-domain message
 					
 			if(nowDomain.equals("Food")) {//Food domain
-				nowIntent = foodIntentClassifier.getLabel(nowInputText);
+				//nowIntent = foodIntentClassifier.getLabel(nowInputText);
 			}else if(nowDomain.equals("Weather")) {//Weather domain
 				nowIntent = weatherIntentClassifier.getLabel(nowInputText);
 			}else {//this shouldn't happen
@@ -74,11 +102,45 @@ public class Chatbot {
 		System.out.println("Intent: "+nowIntent);
 		String nowResponse = "Domain = "+nowDomain+"; Intent = "+nowIntent;
 		
+		//=====Code Added for Assignment 4 (Language Understanding) Begins=====
+		nowResponse += slotTableToString(extractedSlotValues);
+		//=====Code Added for Assignment 4 (Language Understanding) Ends=====
+		
+		
 		return nowResponse;
 		
 		//=====Code Added for Assignment 3 (Language Understanding) Ends=====
 		
 	}
+	
+	/*
+	 * Method slotTableToString() is added for Assignment 4
+	 * 
+	 * [Input]
+	 * A Hashtable<String, String> returned by extractSlotValues() in
+	 * SlotFiller.java
+	 * 
+	 * [Output]
+	 * A string that list all the extracted slot values
+	 * 
+	 */
+	private String slotTableToString(Hashtable<String, String> extractedSlotValues) {
+		
+		String result = " (";
+		
+		for(String nowKey: extractedSlotValues.keySet()) {
+			
+			String nowValue = extractedSlotValues.get(nowKey);
+			System.out.println(nowKey+"="+nowValue);
+			result += nowKey+"="+nowValue+"; ";
+		}
+		
+		result += ")";
+		
+		return result;
+		
+	}
+	
 	
 	public String getUserName() {
 		return userName;
